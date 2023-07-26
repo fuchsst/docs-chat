b336529f58f40a185fc196d6bfb53e2cd757c264
fuchsst <fuchsst>
2023-07-26 20:52:17 +0200

    added docs repos config

--- index_docs.py
+++ index_docs.py
@@ -1,5 +1,8 @@
 import fnmatch
 import os
+from pathlib import Path
+import shutil
+import subprocess
 from langchain import FAISS
 import llama_index
 from llama_index import Document
@@ -9,12 +12,61 @@ from langchain.schema import Document
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import MarkdownTextSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+import yaml
 
 # embedding_model = INSTRUCTOR("hkunlp/instructor-large")
 
 persist_dir = "./docs_vector_storage"
 model_name = "dolly-v2"
 model_id = "databricks/dolly-v2-7b"
+
+def load_repo_configs(config_filename: str):
+    with open(config_filename, 'r') as file:
+        return yaml.safe_load(file)
+
+def git_checkout(repo: str, branch: str, directories: List[str], target_directory:str):
+    def on_readonly_file_error_handler(func, path, exc_info):
+        """
+        Error handler for ``shutil.rmtree``.
+
+        If the error is due to an access error (read only file)
+        it attempts to add write permission and then retries.
+
+        If the error is for another reason it re-raises the error.
+        
+        Usage : ``shutil.rmtree(path, onerror=onerror)``
+        """
+        import stat
+        # Is the error an access error?
+        if not os.access(path, os.W_OK):
+            os.chmod(path, stat.S_IWUSR)
+            func(path)
+        else:
+            raise
+
+
+    print(f'Checkout {repo}/{branch} to {target_directory}')
+    try:
+        # Check if the target directory exists, and delete it if it does
+        if os.path.exists(target_directory):
+            print(f"{target_directory} already present, deleting!")
+            shutil.rmtree(target_directory, onerror=on_readonly_file_error_handler)
+
+        os.mkdir(target_directory)
+        os.chdir(target_directory)
+        subprocess.run(['git', 'init'])
+        subprocess.run(['git', 'remote', 'add', 'origin', repo])
+        subprocess.run(['git', 'config', 'core.sparsecheckout', 'true'])
+        with open('.git/info/sparse-checkout', 'w') as fp:
+            fp.writelines(directories)
+        subprocess.run(['git', 'pull', 'origin', branch])
+        os.chdir('..')
+        
+        print(f"Successfully cloned the '{branch}' branch from {repo} to '{target_directory}'.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
+        if os.path.exists(target_directory):
+            shutil.rmtree(target_directory)
 
 
 def filter_files(directory: str, pattern: str = "*.*") -> List[str]:
@@ -30,7 +82,7 @@ def load_md_docs(
     directory: str, extra_info: Dict[str, str]
 ) -> List[llama_index.Document]:
     #text_splitter = MarkdownTextSplitter(chunk_size=128, chunk_overlap=16)
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=24)
+    text_splitter = RecursiveCharacterTextSplitter(separators=['\n#', '```', '---', '  \n', '\n\n\n'], chunk_size=256, chunk_overlap=24)
     md_files = filter_files(directory, "*.md")
     all_docs: List[llama_index.Document] = []
     print(f"Found {len(md_files)} markdown files in {directory}")
@@ -76,30 +128,37 @@ def persist_docs(docs: List[llama_index.Document], index_name: str):
         Document(page_content=doc.text, metadata=doc.extra_info) for doc in docs
     ]
 
-    print(f"Adding {len(docs)} documents")
+    print(f"Adding {len(docs)} documents to FAISS vectorstore")
 
     vectorstore = FAISS.from_documents(langchain_docs, HuggingFaceEmbeddings())
     vectorstore.save_local(folder_path=db_dir, index_name=index_name)
 
 
 def main():
-    docs = load_md_docs(
-        "./docs_data/argo-workflows/",
-        {
-            "source": "Argo Workflows Documentation",
-            "source_url": "https://github.com/argoproj/argo-workflows/tree/master/docs",
-        },
-    )
-    persist_docs(docs, index_name="argo_workflow_docs")
-
-    docs = load_md_docs(
-        "./docs_data/dbt/",
-        {
-            "source": "dbt Documentation",
-            "source_url": "https://github.com/dbt-labs/docs.getdbt.com/tree/current/website/docs",
-        },
-    )
-    persist_docs(docs, index_name="dbt_docs")
+    repo_configs = load_repo_configs('./doc_source_repos.yml')['git-repos']
+    print(f'Fetching markdown files from {repo_configs.keys()}')
+    all_docs = []
+    base_path= os.getcwd()
+    for repo_key in repo_configs:
+        print(f'# Adding documentation from {repo_key}')
+        repo_conf=repo_configs[repo_key]
+        target_directory=os.path.join('docs_data', repo_key)
+        directories=repo_conf['directories']
+        git_checkout(repo=repo_conf['repo'], branch=repo_conf['branch'], directories=directories, target_directory=target_directory)
+        os.chdir(base_path)
+        for dir in directories:
+            print(f'## Loading markdown files from {target_directory}{dir}')
+            docs = load_md_docs(
+                                target_directory+dir,
+                                {
+                                    "source": repo_conf['description'],
+                                    "source_url": repo_conf['url']+dir,
+                                }
+                            )
+            print(f'## Got {len(docs)} document chunks')
+            all_docs.extend(docs)
+    persist_docs(all_docs, index_name="docs")
+    print("DONE")
 
 
 if __name__ == "__main__":
